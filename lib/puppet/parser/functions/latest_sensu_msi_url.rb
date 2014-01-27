482da3e86a9873d1474d2ab8c020f4e59a09587e
Wouter Scheele <w.scheele@secondfloor.com>
2014-01-27 02:34:09 +0100

    add missing [] arguments to function_ call

--- lib/puppet/parser/functions/latest_sensu_msi_url.rb
+++ lib/puppet/parser/functions/latest_sensu_msi_url.rb
@@ -1,8 +1,8 @@
 module Puppet::Parser::Functions
   newfunction(:latest_sensu_msi_url, :type => :rvalue) do
     url = 'http://repos.sensuapp.org/'
-    Puppet::Parser::Functions.function('latest_sensu_msi_version')
-    latest_version = function_latest_sensu_msi_version()
+    Puppet::Parser::Functions.function(:latest_sensu_msi_version)
+    latest_version = function_latest_sensu_msi_version([])
     "#{url}/msi/sensu-#{latest_version}.msi"
   end
 end
